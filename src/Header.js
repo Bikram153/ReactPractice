cab5b8b8f57098ca01cbd42cff04149d4dd0dd5f
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-06-14 00:10:36 +0530

    Fix the navbar

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,15 @@ import Style from "./style.css"
 
 export default function Header() {
     return(
-            <header>
-                <nav ClassName="nav">
-                    <img src={Logo} className="nav-logo" />
-                    <ul className="nav-items">
-                        <li>Pricing</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
-            </header>
+        <header>
+            <nav className="nav">
+                <img src={Logo} className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
     )
 }
